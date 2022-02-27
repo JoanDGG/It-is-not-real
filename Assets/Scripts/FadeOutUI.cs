4e0a495dc5c47b77b280b495d9a56d55db306e91
JoanDGG <ggjoan2000@gmail.com>
2022-02-27 01:30:28 -0600

    Title scene

--- Assets/Scripts/FadeOutUI.cs
+++ Assets/Scripts/FadeOutUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class FadeOutUI : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class FadeOutUI : MonoBehaviour
     private Text texto;
     public GameObject titleText;
     public GameObject controlsText;
+    private bool IsAvailable = true;
+    public float CooldownDuration;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +25,28 @@ public class FadeOutUI : MonoBehaviour
             StartCoroutine(lifeTimeCoroutine());
             titleText.SetActive(false);
             controlsText.SetActive(false);
+        }
+        else if(gameObject.name == "CenterText")
+        {
+            texto.color = new Color(texto.color.r,
+                                    texto.color.g,
+                                    texto.color.b, 0f);
+            FadeInText();
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if(SceneManager.GetActiveScene().name != "Main")
+            {
+                if (IsAvailable == true)
+                    SceneManager.LoadScene("Main");
+            }
+            else
+            {
+                gameObject.GetComponent<Animator>().SetTrigger("Start");
+            }
         }
     }
 
@@ -40,10 +65,14 @@ public class FadeOutUI : MonoBehaviour
 
     private IEnumerator lifeTimeCoroutine()
     {
+        CooldownDuration = lifeTime + 2.0f;
+        StartCoroutine(StartCooldown());
+
         yield return new WaitForSeconds(2.5f);
         FadeOutText();
         yield return new WaitForSeconds(lifeTime/2);
-        texto.text = "Press space to start";
+        texto.text = "";
+        print("B");
         titleText.SetActive(true);
         controlsText.SetActive(true);
     }
@@ -105,5 +134,13 @@ public class FadeOutUI : MonoBehaviour
                                     texto.color.b, 0f);
             FadeInText();
         }
+    }
+    private IEnumerator StartCooldown()
+    {
+        IsAvailable = false;
+        yield return new WaitForSeconds(CooldownDuration);
+        print("A");
+        texto.text = "Press space to start";
+        IsAvailable = true;
     }
 }
